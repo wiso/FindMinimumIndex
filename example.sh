b201ebeaacca6984a1907d7251e72cb86b4a45d8
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2019-12-06 16:27:58 +0000

    add example.sh

---
+++ example.sh
@@ -0,0 +1,6 @@
+cd benchmark/
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release  -DBENCHMARK_ENABLE_GTEST_TESTS=OFF ../
+cd ../../
+g++ findMinumumIndex_bench.cxx -std=c++17 -march=x86-64  -O3 -isystem benchmark/include -Lbenchmark/build/src -lbenchmark -lpthread -o mybenchmark
+./benchmark
